5a23e829ecc5d8cd5b544b325be575764800b7ec
Ariyan0047 <aariyan0047@gmail.com>
2021-03-31 09:54:51 +0600

    insert data in the database

--- templates/process.php
+++ templates/process.php
@@ -3,4 +3,15 @@
 ($connection = new mysqli("localhost", "root", "", "crud")) or
   die(mysqli_error($connection));
 
+if (isset($_POST["submit"])) {
+  $date = $_POST["date"];
+  $amount = $_POST["amount"];
+  $category = $_POST["category"];
+
+  $insertSql = "INSERT INTO data(amount,category,created_at) VALUES('$amount','$category','$date')";
+  if (mysqli_query($connection, $insertSql)) {
+    header("Location: ../index.php");
+  }
+}
+
 ?>
